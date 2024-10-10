80ea33797397813f5dda2bd76b05bb6f9483b464
Doehyun Baek <doehyunbaek@gmail.com>
2024-10-10 16:29:35 +0900

    add script.sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bah
+
+if [ -z "$BINARYEN_ROOT" ]; then
+    echo "Error: BINARYEN_ROOT environment variable is not set"
+    echo "https://github.com/WebAssembly/binaryen/blob/871ff0d4f910b565c15f82e8f3c9aa769b01d286/src/support/path.cpp#L95"
+    exit 1
+fi
+
+wasm-reduce -to 60 -b $BINARYEN_ROOT/bin '--command=./oracle.py ./test.wasm' -t ./test.wasm -w ./work.wasm ./boa.wasm
